a2e013b12d1b9f3bbb42e80b6df4ce6f1832db01
Xander Bailey <xander.bailey@gmail.com>
2023-02-09 16:20:57 -0800

    Remove test function, move to CLI

--- llist.c
+++ llist.c
@@ -24,7 +24,7 @@ void llist_insert_tail(struct node** head, struct node* n) {
         return;
     }
     if ((*head)->next != NULL) llist_insert_tail(&(*head)->next, n);
-    (*head)->next = n;
+    else (*head)->next = n;
 }
 
 
@@ -68,88 +68,36 @@ void node_free(struct node* n) {
 }
 
 
-void test(void) {
-    struct node* head = node_alloc(123);
-    printf("First node added:\n");
-    llist_print(head);
-
-    struct node* new_node = node_alloc(456);
-    llist_insert_head(&head, new_node);
-    head = new_node;
-    printf("2nd node added:\n");
-    llist_print(head);
-
-    head = llist_delete_head(&head);
-    printf("removed head:\n");
-    llist_print(head);
-
-    llist_insert_tail(&head, new_node);
-    printf("added new tail:\n");
-    llist_print(head);
-
-    llist_free(&head);
-    printf("freed list:\n");
-    llist_print(head);
-
-    head = NULL;
-    new_node = node_alloc(123);
-
-    llist_insert_head(&head, new_node);
-    head = new_node;
-    printf("added head to empty:\n");
-    llist_print(head);
-
-    llist_free(&head);
-
-    head = NULL;
-    new_node = node_alloc(123);
-
-    llist_insert_tail(&head, new_node);
-    printf("added tail to empty:\n");
-    llist_print(head);
-
-    llist_free(&head);
-    printf("freed list:\n");
-    llist_print(head);
-
-    llist_free(&head);
-    printf("freed list again:\n");
-    llist_print(head);
-}
-
-
 int main(int argc, char* argv[])
 {
     struct node* head = NULL;
 
-    test();
-    exit(0);
-
     for (int i = 0; i < argc; i++)
     {
-        if (strcmp(argv[i], "ih")) {
+        if (strcmp(argv[i], "ih") == 0) {
             if (i + 1 >= argc) printf("Command 'ih' missing requirement");
             i++;
 
             int value = atoi(argv[i]);
 
             struct node* new_node = node_alloc(value);
-            // llist_insert_head(&head, new_node);
+            llist_insert_head(&head, new_node);
             head = new_node;
-        } else if (strcmp(argv[i], "it")) {
+        } else if (strcmp(argv[i], "it") == 0) {
             if (i + 1 >= argc) printf("Command 'it' missing requirement");
             i++;
 
             int value = atoi(argv[i]);
 
             struct node* new_node = node_alloc(value);
-            // llist_insert_tail(&head, new_node);
-        } else if (strcmp(argv[i], "dh")) {
-            // struct node* old_head = llist_delete_head(&head);
-            // node_free(old_head);
-        } else if (strcmp(argv[i], "f")) {
-            // llist_free(&head);
-        } else if (strcmp(argv[i], "p")) {
+            llist_insert_tail(&head, new_node);
+        } else if (strcmp(argv[i], "dh") == 0) {
+            struct node* old_head = head;
+            head = llist_delete_head(&head);
+            node_free(old_head);
+        } else if (strcmp(argv[i], "f") == 0) {
+            llist_free(&head);
+        } else if (strcmp(argv[i], "p") == 0) {
             llist_print(head);
         }
     }
